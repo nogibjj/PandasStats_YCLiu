5ccb91e4e792327f01d6b3ba2dfd383719a6b30b
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2023-09-09 21:59:24 +0000

    create statsYC.py

---
+++ statsYC.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def calMean(df, clmn):
+    if clmn not in df.columns:
+        raise ValueError("ValueError. Input column not in input DataFrame.")
+    try:
+        return df[clmn].mean()
+    except:
+        raise ValueError("ValueError. Check if input column is of datatype int or float.")
+    
+
+def calMedian(df, clmn):
+    if clmn not in df.columns:
+        raise ValueError("ValueError. Input column not in input DataFrame.")
+    try:
+        return df[clmn].median()
+    except:
+        raise ValueError("ValueError. Check if input column is of datatype int or float.")
+
+def countItemOcc(df, clmn, example):
+    if clmn not in df.columns:
+        raise ValueError("ValueError. Input column not in input DataFrame.")
+    return df[df[clmn]==example].shape[0]
+
+def calItemRate(df, clmn, example):
+    if clmn not in df.columns:
+        raise ValueError("ValueError. Input column not in input DataFrame.")
+    return (df[df[clmn]==example].shape[0])/(df.dropna(subset=[clmn]).shape[0])
+
+def printNumClmnStats(df, clmn):
+    clmnMean = round(calMean(df, clmn), 2)
+    clmnMedian = round(calMedian(df, clmn), 2)
+    print('The mean is {}. The median is {}.'.format(str(clmnMean),str(clmnMedian)))
+    return None
+          
+def printCatClmnStats(df, clmn, example):
+    ItemOcc = countItemOcc(df, clmn, example)
+    ItemOccPercent = calItemRate(df, clmn, example)
+    print('The number of occurance is {}, or {}% of total samples.'.format(ItemOcc,str(round(ItemOccPercent, 2))))
+    return None
